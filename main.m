1a1180e2ac80b39b205b638c6d7db4a54ca850fb
AnaisPaulin <36475912+AnaisPaulin@users.noreply.github.com>
2018-04-26 15:08:09 +0200

    transformation boucle q3 en while

--- main.m
+++ main.m
@@ -80,13 +80,13 @@ for i=1:length(m_2)
 end
 
 figure()
-plot((m_2-4)/2,P_available-P)
+plot(m_2-4,P_available-P)
 title('Difference between the available and the required power')
 xlabel('Number of batteries')
 ylabel('Power difference [W]')
 
 figure()
-plot((m_2-4)/2,time_hover_2)
+plot(m_2-4,time_hover_2)
 title('Hovering time')
 xlabel('Number of batteries')
 ylabel('Hovering time [s]')
@@ -110,7 +110,7 @@ theta(2,:) = linspace(theta_ref,theta_tip,N); %linear
 theta(3,:) = theta_tip./r; %not AOA_0 but theta_tip
 
 theta_tw = [0 ; (theta_tip - theta_ref);(theta_tip*(1 - 1/0.6)/0.4)]; %pente
- %theta 3/4R
+
 
 
 
@@ -122,7 +122,7 @@ theta_simple = theta_tip./r;
 max_it = 100; %maximum number of iterations
 
 dC_T = zeros(max_it+1,N);
-C_T = trapz(dC_T);
+C_T(1) = trapz(r,dC_T(1,:));
 F = ones(max_it+1,N);
 
 i1 = 3; %type of twist
@@ -130,26 +130,24 @@ i1 = 3; %type of twist
 theta0(1) = 6*C_Treq./(sigma*2*pi) - 3/4*theta_tw(i1) + 3/2*sqrt(C_Treq/2);
 theta3_lin = (theta_tip*(1 - 1/0.6)/0.4)*r + theta0(1); %use that
 theta(3,:) = theta3_lin; 
-
-for i2 =1:max_it
-        theta0(i2+1,:) = theta0(i2,:) + (6*(C_Treq - C_T(i2))./(sigma*2*pi) ...
+i2=1;
+while i2<max_it && abs(C_T(i2)-C_Treq)>10^(-3)
+        
+        theta0(i2+1) = theta0(i2) + (6*(C_Treq - C_T(i2))./(sigma*2*pi) ...
             + 3*sqrt(2)/4*(sqrt(C_Treq)-sqrt(C_T(i2))));
-        theta(i1,:) = theta0(i2,:) + theta_tw(i1).*r;
-        theta_75 = theta(:,15);
+        theta(i1,:) = theta0(i2) + theta_tw(i1).*r;
+        theta_75 = theta(:,15); %theta 3/4R
         lambda(i2,:) = sigma*2*pi./(16.*F(i2,:)).*(sqrt(1 +  ... 
             32.*F(i2,:)./(sigma*2*pi).*theta(i1,:).*r) - 1);
         phi = lambda(i2,:)./r;
         f(i2,:) = N_b/2.*(1-r)./(r.*phi);
         F(i2+1,:) = 2/pi .* acos(exp(-f(i2)));
-        %dC_T(i2,:) = 1/2*sigma*2*pi.*r.^2;
-        dC_T(i2,:) = 1/2*sigma.*2*pi.*(theta_75(i1)./3 - lambda(i2,:)/2);
-        C_T(i2) = trapz(r,dC_T(i2,:));
-        if abs(C_T(i2)-C_Treq) < 10^(-3)
-            C_T_sol = C_T(i2);
-            break
-        end
-        T_2(i2) = (4*rho*pi*R^4*omega^2)*C_T(i2);
+        %dC_T(i2+1,:) = 1/2*sigma*2*pi.*r.^2;
+        dC_T(i2+1,:) = 1/2*sigma.*2*pi.*(theta_75(i1)./3 - lambda(i2,:)/2);
+        C_T(i2+1) = trapz(r,dC_T(i2+1,:));
 
+        T_2(i2) = (4*rho*pi*R^4*omega^2)*C_T(i2+1);
+i2=i2+1;
 end
 
 figure()
@@ -171,4 +169,4 @@ for i =1:3
     Cl(i,:) = interp1(alpha_ref,Cl_ref,theta(i,:));
     Cd(i,:) = interp1(alpha_ref,Cd_ref,theta(i,:));
 end
-
+    
