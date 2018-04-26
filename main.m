74154712bd96d24ea0daa91c47b55d0dc5a3d95f
abft <38354561+abft@users.noreply.github.com>
2018-04-26 10:02:04 +0200

    jeudi10h

--- main.m
+++ main.m
@@ -20,7 +20,7 @@ omega = V_tip./R;
 %omega = [200:50:450]; %rad/s
 kappa = 1.2; %cf litterature
 C_d0 = 0.011;
-T= m*g; %per rotor
+T= m*g/4; %per rotor
 mini_P = inf;
 
 for i1=1:length(N_b)
@@ -68,25 +68,25 @@ m_2 = [6:1:20]; %total mass with added batteries
 clear P
 
 for i=1:length(m_2)
-    T= m_2(i)*g;
+    T= m_2(i)*g/4;
     
     P_ideal = T^(3/2)/sqrt(2*rho*pi*R^2);
     P_0 = 1/8*rho*N_b*omega^3*chord*C_d0*R^4;
     P(i) = kappa*P_ideal + P_0; %required power
     
     P_available(i) = 500*(m_2(i)-4); %mass of batteries
-    time_hover_2(i)=E*(m_2(i)-4)/P(i);
+    time_hover_2(i)=E*(m_2(i)-4)/(4*P(i));
 
 end
 
 figure()
-plot(m_2-4,P_available-P)
+plot((m_2-4)/2,P_available-P)
 title('Difference between the available and the required power')
 xlabel('Number of batteries')
 ylabel('Power difference [W]')
 
 figure()
-plot(m_2-4,time_hover_2)
+plot((m_2-4)/2,time_hover_2)
 title('Hovering time')
 xlabel('Number of batteries')
 ylabel('Hovering time [s]')
@@ -134,9 +134,9 @@ end
 
 %pour avoir les theta doit on enlever phi ?
 
-theta_tw = AOA(:,15)./r;
+theta_tw = AOA./R; %dépend de r ?
 theta_75 = AOA(:,15); %theta 3/4R
-C_Treq = T/(rho*pi*R^4*omega^2);
+C_Treq = T/(4*rho*pi*R^4*omega^2);
 
 max_it = 100; %maximum number of iterations
 
@@ -144,9 +144,9 @@ C_T = zeros(max_it+1,N);
 F = ones(1,max_it+1);
 
 i1 = 2; % linear blade
-theta0(1,:) = 6*C_Treq./(sigma.*Cl(i1,:)) - 3/4*theta_tw(i1,:) + 3/2*sqrt(C_Treq/2);
-for i2 =2:max_it
-        theta0(i2,:) = theta0(i2-1,:) + (6*(C_Treq - C_T(i2))./(sigma.*Cl(i1,:)) ...
+theta0(1,:) = 6*C_Treq./(sigma.*Cl(i1,:)) - 3/4*theta_tw(i1) + 3/2*sqrt(C_Treq/2);
+for i2 =1:max_it
+        theta0(i2+1,:) = theta0(i2,:) + (6*(C_Treq - C_T(i2))./(sigma.*Cl(i1,:)) ...
             + 3*sqrt(2)/4*(sqrt(C_Treq)-sqrt(C_T(i2))));
         theta(i2,:) = theta0(i2,:) + AOA(i1,:);
         lambda(i2,:) = sigma.*Cl(i1,:)/(16*F(i2)).*(sqrt(1 +  ... 
@@ -154,7 +154,7 @@ for i2 =2:max_it
         phi_tip = lambda(i2,end)/R;
         f(i2) = N_b/2*(1-r)/(r*phi_tip);
         F(i2+1) = 2/pi * acos(exp(-f(i2)));
-        C_T(i2+1,:) = 1/2*sigma.*Cl(i1,:)*(theta_75(i1)./3 - lambda(i2,:)/2) ;
+        C_T(i2,:) = 1/2*sigma.*Cl(i1,:).*(theta_75(i1)./3 - lambda(i2,:)/2) ;
 %         if C_T == C_Treq
 %             break
 %         end
